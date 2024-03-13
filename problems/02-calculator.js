ea5db18a3007a9144c2926d9dca930b62a001c2c
abraham <agarciasantos14@gmail.com>
2024-03-12 19:54:44 -0700

    completed 2

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,6 +1,34 @@
-// Your code here 
+// Your code here
+class Calculator {
+	constructor(total){
+		this.total = 0
+	}
+
+	add(num){
+		return this.total = num + this.total
+	}
+
+	subtract(num){
+		return this.total -= num
+
+	}
+
+	divide(num){
+		return this.total = this.total/num
+	}
+
+	multiply(num){
+		return this.total = this.total * num
+	}
+}
 
 
+let calculator = new Calculator();
+console.log(calculator.add(50));      // => 50
+console.log(calculator.subtract(35)); // => 15
+console.log(calculator.multiply(10)); // => 150
+console.log(calculator.divide(5));    // => 30
+console.log(calculator.total)         // => 30
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
