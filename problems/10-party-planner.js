6bf74785da37638985cbf438316948c93118cb1e
abraham <agarciasantos14@gmail.com>
2024-03-12 21:17:25 -0700

    completed 10

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,30 @@
-// Your code here 
+// Your code here
+class PartyPlanner {
+	constructor (guestList){
+		this.guestList = []
+	}
+	addToGuestList(name){
+		this.guestList.push(name)
+	}
+	throwParty(){
+		if (!this.guestList.length){
+			return "Gotta add people to the guest list"
+		}else {
+				return `Welcome to the party ${this.guestList.join(' and ')}`
+		}
+	}
+}
 
+
+const party = new PartyPlanner();
+
+console.log(party.throwParty()); // prints "Gotta add people to the guest list"
+
+party.addToGuestList("James");
+console.log(party.throwParty()); // prints "Welcome to the party James"
+
+party.addToGuestList("Alvin");
+console.log(party.throwParty()); // prints "Welcome to the party James and Alvin"
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
