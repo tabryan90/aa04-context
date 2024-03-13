208bb80d219633bbd6bd9f35b099b6dd7467df63
Alex Bryan <tabryan1990@gmail.com>
2024-03-12 22:44:51 -0400

    phase 1 complete

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -1,4 +1,26 @@
-// Your code here 
+// Your code here
+class Car {
+	constructor(speed){
+		this.speed = 0;
+	}
+
+	drive(newSpeed) {
+		return this.speed = newSpeed;
+	}
+}
+
+let car = new Car();
+car.drive(0);     // => returns 0
+console.log(car); // => { speed: 0 }
+
+car.drive(10);    // => returns 10
+console.log(car); // => { speed: 10 }
+
+car.drive(50);    // => returns 50
+console.log(car); // -> { speed: 50 }
+
+car.drive(100);   // => returns 100
+console.log(car); // -> { speed: 100 }
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
